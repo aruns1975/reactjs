f9a25b73aaa415296eda1cdab8e577d43622f777
Arun Subramonia Sarma <aruns1975@gmail.com>
2018-09-08 09:02:26 +0530

    Child Component and Life cycle methods

--- README.md
+++ README.md
@@ -1,34 +1,55 @@
 # reactjs
 
-Session 2 Notes
+Session 3 Notes
+----------------
+Child Component
+1) Creating child component
+2) Parent component can pass props to the child component
+3) The child component has a special property called as children
+4) Functional component using functions instead of class
+5) Functional components are stateless and can receive props
+6) The child component can receive the event handler of the parent as props
+7) The child component can call the received event handler to change the state of the parent component
 
-class MyClass{
-	myVar = 'Test';
-	myMethod =function(){
-		this.myVar = 'New Test';
-		console.log('I am inside the method');
-	}
+Life Cycle Method of Component
+1) Creation Life Cycle
+	1) constructor
+	2) getInitialState
+	3) componentWillMount
+	4) render
+		a) Child component lifecycle will begin
+	5) componentDidMount
 	
-	MyAotherMethd = () =>{
-		this.myVar  =' another var'
-	}
-}
+2) Update Life cycle - Internal (state change)
+	1) shouldComponentUpdate
+	2) componentWillUpdate (No side effects)
+	3) render (No side effects)
+		a) Child component lifecycle will begin
+	4) componentDidUpdate (Side effects -changing state will trigger render)
+	
+3) Update Life cycle - External (Props change)
+	1) componentWillReceiveProps (Setting the state of the child component with the props received)
+	2) shouldComponentUpdate (Returning false will prevent subsequent life cycle being called)
+	3) componentWillUpdate (No side effects)
+	4) render
+		a) Child component lifecycle will begin
+	5) componentDidUpdate (Side effects -changing state will trigger render)
 
-let obj1=new MyClass();
+Note-	Functional component does not have a life cycle methods
 
-
-obj1.myMethod();
-
-let myMethod= obj1.myMethod();
-let myMethod1= obj1.MyAotherMethd();
-
-myMethod();
-
-class MyClass2{
-	myMthod(){
-		let obj1=new MyClass();
-
-		let myMethod= obj1.myMethod().bind(obj1);
-		myMethod();
-	}
-}
+PureComponent
+	extending pureComponent
+		shouldComponentUpdate method is already been provided where is the setState changes the same value, the method return false
+			this prevents subsequent life cycle being called
+			
+			//works
+			state{
+				state1:'sdsdsd'
+				state2:'sdsddsfdsf'
+			}
+			
+			//doesnot work as per your expectation
+			state{
+				state1: {dsd:dfddsf,dsfdsf:fdgfdg}
+				state2: {dsdsd:ytrgg,dfdsfdsf:fgtrgrg}
+			}
--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
 import './App.css';
 
 class App extends Component {
@@ -8,6 +8,38 @@ class App extends Component {
       width:0,
       height:10
   };
+  //Creation life cycle method
+  componentWillMount = ()=>{
+    console.log('[App]','Inside component will mount');
+  }
+  //Creation life cycle method
+  componentDidMount=()=>{
+    console.log('[App]','Inside component did mount');
+    this.setState(
+      {greeting: 'Hello, World'}
+    )
+  }
+  //Update life cycle method - extenal
+  componentWillReceiveProps =(nextProps) =>{
+    console.log('[App]','Inside component receives props',nextProps);
+  }
+  //Update life cycle method - internal and extenal
+  shouldComponentUpdate = (nextProps,nextState)=>{
+    console.log('[App]','Inside should component update',nextState, nextState);
+    console.log(nextState.greeting)
+    console.log(this.state.greeting)
+    return (nextState.greeting !== this.state.greeting)
+  }
+  //Update life cycle method - internal and extenal
+  componentWillUpdate = (nextProps, nextState) => {
+    console.log('[App]','Inside component will update', nextProps, nextState);
+  }
+   //Update life cycle method - internal and extenal
+   componentDidUpdate = (nextProps, nextState) => {
+    console.log('[App]','Inside component did update', nextProps, nextState);
+  }
+  
+
   greetArun = ()=>{
     console.log("I am fired");
     this.setState({
@@ -28,19 +60,75 @@ class App extends Component {
     });
   }
   render() {
-    console.log("I am render method");
+    console.log('[APP]',"I am render method");
     return (
       <div className="App">
-      {this.state.greeting} <br/>> 
-       <input type="text" value={this.state.greeting} onChange={this.nameChangeHandler}/>
-       <input type="text" value={this.state.greeting} readOnly/>
-       <button onClick={this.greetArun}>Arun</button>
-       <button onClick={this.display}>Display</button><br/>
-       Another Greet : {this.state.anotherGreeting}
+        {this.state.greeting} <br/> 
+        <input type="text" value={this.state.greeting} onChange={this.nameChangeHandler}/>
+        <input type="text" value={this.state.greeting} readOnly/>
+        <button onClick={this.greetArun}>Arun</button>
+        <button onClick={this.display}>Display</button><br/>
+        Another Greet : {this.state.anotherGreeting}
+        <hr/>
+        <Div greeting={this.state.greeting} nameChangeHandler={this.nameChangeHandler} changeState={this.greetArun}><h1>{this.state.greeting}</h1></Div>
+        <hr/>
+        <DIV greeting={this.state.greeting} nameChangeHandler={this.nameChangeHandler} changeState={this.greetArun}><h1>{this.state.greeting}</h1></DIV>
       </div>
     );
   }
 }
 
+//Statefull Component
+class Div extends Component{
+  componentWillMount = ()=>{
+    console.log('   [Div]','Inside component will mount');
+  }
+  componentDidMount=()=>{
+    console.log('   [Div]','Inside component did mount');
+    this.setState(
+      {greeting: 'Hello, World'}
+    )
+  }
+  //Update life cycle method - extenal
+  componentWillReceiveProps =(nextProps) =>{
+    console.log('     [Div]','Inside component receives props',nextProps);
+    this.setState(nextProps);
+  }
+  //Update life cycle method - internal and extenal
+  shouldComponentUpdate = (nextProps, nextState)=>{
+    console.log('    [Div]','Inside should component update');
+    return true;
+  }
+  //Update life cycle method - internal and extenal
+  componentWillUpdate = (nextProps, nextState) => {
+    console.log('     [Div]','Inside component will update', nextProps, nextState);
+  }
+   //Update life cycle method - internal and extenal
+   componentDidUpdate = (nextProps, nextState) => {
+    console.log('      [Div]','Inside component did update', nextProps, nextState);
+  }
+
+  render(){
+    console.log('   [Div]',"I am render method");
+    return <div>
+              {this.props.children}
+              <input type="text" value={this.props.greeting} onChange={this.props.nameChangeHandler}/>
+              <button onClick={this.props.changeState}>Change State</button>
+            </div>
+  }
+}
+
+class MyPureComponent extends PureComponent{
+
+}
+//Functional component or stateless component
+const DIV=(props)=>{
+  console.log('    [DIV]',"I am render method");
+  return <div>
+          {props.children}
+          <input type="text" value={props.greeting} onChange={props.nameChangeHandler}/>
+          <button onClick={props.changeState}>Change State</button>
+        </div>
+}
 export default App;
 
