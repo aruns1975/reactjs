b4843b3b8068156f1f188652c6c89ba9f70bc49f
Arun Subramonia Sarma <aruns1975@gmail.com>
2018-09-09 09:01:00 +0530

    HOC

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,134 +1,76 @@
-import React, { Component, PureComponent } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
   state={
-      greeting: 'Hello, World',
-      anotherGreeting:'',
-      width:0,
-      height:10
+      emps:[
+        {id:1,name:'Arun',age:23,salary:10000},
+        {id:2,name:'Raghu',age:25,salary:14000},
+        {id:3,name:'Kumar',age:34,salary:10600},
+        {id:4,name:'Ravi',age:29,salary:10032},
+      ],
+      depts:[
+        {id:1,name:'Accounts',loc:'First Floor'}
+      ]
   };
-  //Creation life cycle method
-  componentWillMount = ()=>{
-    console.log('[App]','Inside component will mount');
-  }
-  //Creation life cycle method
-  componentDidMount=()=>{
-    console.log('[App]','Inside component did mount');
-    this.setState(
-      {greeting: 'Hello, World'}
-    )
-  }
-  //Update life cycle method - extenal
-  componentWillReceiveProps =(nextProps) =>{
-    console.log('[App]','Inside component receives props',nextProps);
-  }
-  //Update life cycle method - internal and extenal
-  shouldComponentUpdate = (nextProps,nextState)=>{
-    console.log('[App]','Inside should component update',nextState, nextState);
-    console.log(nextState.greeting)
-    console.log(this.state.greeting)
-    return (nextState.greeting !== this.state.greeting)
-  }
-  //Update life cycle method - internal and extenal
-  componentWillUpdate = (nextProps, nextState) => {
-    console.log('[App]','Inside component will update', nextProps, nextState);
-  }
-   //Update life cycle method - internal and extenal
-   componentDidUpdate = (nextProps, nextState) => {
-    console.log('[App]','Inside component did update', nextProps, nextState);
-  }
-  
-
-  greetArun = ()=>{
-    console.log("I am fired");
-    this.setState({
-        greeting: 'Hello, Arun'
-    });
-    
-  }
-  nameChangeHandler = event => {
-    this.setState({
-      greeting: event.target.value,
-      width:this.state.width+1
-    });
-  }
-  display = () => {
-    console.log('Calling display',this.state.greeting);
-    this.setState({
-      anotherGreeting : this.state.greeting
-    });
-  }
-  render() {
-    console.log('[APP]',"I am render method");
-    return (
-      <div className="App">
-        {this.state.greeting} <br/> 
-        <input type="text" value={this.state.greeting} onChange={this.nameChangeHandler}/>
-        <input type="text" value={this.state.greeting} readOnly/>
-        <button onClick={this.greetArun}>Arun</button>
-        <button onClick={this.display}>Display</button><br/>
-        Another Greet : {this.state.anotherGreeting}
-        <hr/>
-        <Div greeting={this.state.greeting} nameChangeHandler={this.nameChangeHandler} changeState={this.greetArun}><h1>{this.state.greeting}</h1></Div>
-        <hr/>
-        <DIV greeting={this.state.greeting} nameChangeHandler={this.nameChangeHandler} changeState={this.greetArun}><h1>{this.state.greeting}</h1></DIV>
-      </div>
-    );
-  }
-}
-
-//Statefull Component
-class Div extends Component{
-  componentWillMount = ()=>{
-    console.log('   [Div]','Inside component will mount');
-  }
-  componentDidMount=()=>{
-    console.log('   [Div]','Inside component did mount');
-    this.setState(
-      {greeting: 'Hello, World'}
-    )
-  }
-  //Update life cycle method - extenal
-  componentWillReceiveProps =(nextProps) =>{
-    console.log('     [Div]','Inside component receives props',nextProps);
-    this.setState(nextProps);
-  }
-  //Update life cycle method - internal and extenal
-  shouldComponentUpdate = (nextProps, nextState)=>{
-    console.log('    [Div]','Inside should component update');
-    return true;
-  }
-  //Update life cycle method - internal and extenal
-  componentWillUpdate = (nextProps, nextState) => {
-    console.log('     [Div]','Inside component will update', nextProps, nextState);
-  }
-   //Update life cycle method - internal and extenal
-   componentDidUpdate = (nextProps, nextState) => {
-    console.log('      [Div]','Inside component did update', nextProps, nextState);
-  }
-
   render(){
-    console.log('   [Div]',"I am render method");
-    return <div>
-              {this.props.children}
-              <input type="text" value={this.props.greeting} onChange={this.props.nameChangeHandler}/>
-              <button onClick={this.props.changeState}>Change State</button>
-            </div>
+    const emps=this.state.emps.map(emp=>{ 
+      return (
+        <EmpWrapper key={emp.id} id={emp.id} name={emp.name} age={emp.age} salary={emp.salary}></EmpWrapper>
+      )
+    })
+    const depts=this.state.depts.map(dept=>{ 
+      return (
+        <DeptWrapper key={dept.id} dept_id={dept.id} dept_name={dept.name} dept_loc={dept.loc}></DeptWrapper>
+      )
+    })
+    console.log(emps)
+    return (
+      <Dummy>
+        {emps}
+        {depts}
+      </Dummy >
+    )
   }
 }
-
-class MyPureComponent extends PureComponent{
-
+const Dummy=(props) => {
+    return props.children
 }
-//Functional component or stateless component
-const DIV=(props)=>{
-  console.log('    [DIV]',"I am render method");
-  return <div>
-          {props.children}
-          <input type="text" value={props.greeting} onChange={props.nameChangeHandler}/>
-          <button onClick={props.changeState}>Change State</button>
-        </div>
+const Div=(myClass, Children) => {
+  return (props) => (
+     <div className={myClass}>
+      <Children {...props}></Children>
+    </div>
+  )
 }
+class Emp extends Component{
+  render(){
+    console.log('[Emp]',this.props)
+    return (
+      <div>
+        <span>{this.props.id}</span>
+        <span>{this.props.name}</span>
+        <span>{this.props.age}</span>
+        <span>{this.props.salary}</span>
+      </div>
+    )
+  }
+}
+const EmpWrapper=Div('myClass',Emp);
+
+class Dept extends Component{
+  render(){
+    console.log('[Dept]',this.props)
+    return (
+      <div>
+        <span>{this.props.dept_id}</span>
+        <span>{this.props.dept_name}</span>
+        <span>{this.props.dept_loc}</span>
+      </div>
+    )
+  }
+}
+const DeptWrapper=Div('myClass',Dept);
+
 export default App;
 
